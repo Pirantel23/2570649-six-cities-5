6c4495be630701ddb56c59a8e5db9b53c0837f01
Pirantel <max420652@gmail.com>
2025-01-04 22:25:32 +0500

    7.1

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,4 +1,6 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
+import HistoryRouter from '@/components/history-route/history-route';
+import browserHistory from '@/browser-history';
 import {HelmetProvider} from 'react-helmet-async';
 import { AppRoute, AuthStatus } from '@/const';
 import PrivateRoute from '@/components/private-route/private-route';
@@ -7,47 +9,51 @@ import MainScreen from '@/pages/main-screen/main-screen';
 import OfferScreen from '@/pages/offer-screen/offer-screen';
 import FavoritesScreen from '@/pages/favourite-screen/favourite-screen';
 import NotFoundScreen from '@/pages/404/404';
-import { useAppDispatch, useAppSelector } from '@/hooks/index';
-import { setOffersList } from '@/store/action';
+import { useAppSelector } from '@/hooks/index';
+import LoadingScreen from '@/pages/loading-screen/loading-screen';
 
 export default function App(): JSX.Element {
-  const offers = useAppSelector((state) => state.offers);
+  const authorizationStatus = useAppSelector((state) => state.authorizationStatus);
+  const isOffersDataLoading = useAppSelector((state) => state.isOffersDataLoading);
 
-  const dispatch = useAppDispatch();
-  dispatch(setOffersList(offers));
+  if (authorizationStatus === AuthStatus.Unknown || isOffersDataLoading) {
+    return (
+      <LoadingScreen />
+    );
+  }
 
   return (
     <HelmetProvider>
-      <BrowserRouter>
+      <HistoryRouter history={browserHistory}>
         <Routes>
           <Route
             path={AppRoute.Root}
-            element={<MainScreen />}
+            element={<MainScreen/>}
           />
           <Route
             path={AppRoute.Login}
-            element={<LoginScreen />}
+            element={<LoginScreen/>}
           />
           <Route
             path={AppRoute.Favorites}
             element={
               <PrivateRoute
-                authorizationStatus={AuthStatus.Authenticated}
+                authorizationStatus={authorizationStatus}
               >
-                <FavoritesScreen />
+                <FavoritesScreen/>
               </PrivateRoute>
             }
           />
           <Route
             path={`${AppRoute.Offer}/:id`}
-            element={<OfferScreen />}
+            element={<OfferScreen/>}
           />
           <Route
             path='*'
             element={<NotFoundScreen />}
           />
         </Routes>
-      </BrowserRouter>
+      </HistoryRouter>
     </HelmetProvider>
   );
 }
--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -1,35 +1,57 @@
 import { createReducer } from '@reduxjs/toolkit';
-import { setOffersList, setCity, setSortType } from './action';
-import { Offers } from '../types/offer';
-import { offers as mockOffers } from '@/mocks/offers';
-import { DEFAULT_CITY, SortType } from '@/const';
+import { loadOffers, changeCity, loadOffersInDetails , loadReviews, setSortType, requireAuthorization, setError, setOffersDataLoadingStatus } from './action';
+import { AuthStatus, CITIES, SortType } from '@/const';
+import { Offers } from '@/types/offer';
 import { Reviews } from '@/types/review';
 import { City } from '@/types/city';
-
+import { OffersDetailed } from '@/types/offer-detailed';
 
 type StateType = {
   city: City;
   offers: Offers;
+  offersInDetails: OffersDetailed;
   reviews: Reviews;
   sortType: SortType;
+  authorizationStatus: AuthStatus;
+  error: string | null;
+  isOffersDataLoading: boolean;
 };
 
 const initialState: StateType = {
-  city: DEFAULT_CITY,
+  city: CITIES[0],
   offers: [],
+  offersInDetails: [],
   reviews: [],
   sortType: SortType.Popular,
+  authorizationStatus: AuthStatus.Unknown,
+  error: null,
+  isOffersDataLoading: false,
 };
 
 export const reducer = createReducer(initialState, (builder) => {
   builder
-    .addCase(setCity, (state, { payload }) => {
-      state.city = payload;
+    .addCase(changeCity, (state, { payload }) => {
+      state.city = { ...payload };
     })
-    .addCase(setOffersList, (state) => {
-      state.offers = mockOffers;
+    .addCase(loadOffers, (state, action) => {
+      state.offers = action.payload;
+    })
+    .addCase(loadOffersInDetails, (state, action) => {
+      state.offersInDetails = action.payload;
+    })
+    .addCase(loadReviews, (state, action) => {
+      state.reviews = action.payload;
     })
     .addCase(setSortType, (state, { payload }) => {
-      state.sortType = payload})
-    ;
+      state.sortType = payload;
+    })
+    .addCase(requireAuthorization, (state, action) => {
+      state.authorizationStatus = action.payload;
+    })
+    .addCase(setError, (state, action) => {
+      state.error = action.payload;
+    })
+    .addCase(setOffersDataLoadingStatus, (state, action) => {
+      state.isOffersDataLoading = action.payload;
+    });
 });
